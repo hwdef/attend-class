22d62127bb376f7af1b30da7c026e3a0f35bfd39
hwdef <hwdef97@gmail.com>
2020-01-23 23:36:21 +0800

    添加导入信息功能

--- main.py
+++ main.py
@@ -1,15 +1,53 @@
+import os
+import sqlite3
+
 from view.view import Ui_MainWindow
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QFileDialog
 import sys
+
+
+class ui_view(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(ui_view, self).__init__(parent)
+        self.setupUi(self)
+
+    def setupUi(self, MainWindow):
+        Ui_MainWindow.setupUi(self, MainWindow)
+        super().setupUi(MainWindow)
+        self.import_information_button.clicked.connect(self.choose_dir)
+
+    def choose_dir(self):
+        directory = QFileDialog.getExistingDirectory(self, "选取文件夹", "./")
+        if directory != '':
+            data = []
+            files_name = os.listdir(directory)
+            for file_name in files_name:
+                file_name_split = file_name.split(',')
+                file_name_split.append('')
+                data.append(file_name_split)
+            conn = sqlite3.connect('data.db')
+            c = conn.cursor()
+            c.execute('''CREATE TABLE information
+                    (
+                        id text,
+                        name text,
+                        class text,
+                        check_in text
+                    );''')
+            c.executemany("INSERT INTO information VALUES (?,?,?,?)", data)
+            conn.commit()
+            conn.close()
 
 
 def view_show():
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
+    ui = ui_view()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
+
 
 if __name__ == "__main__":
     view_show()
